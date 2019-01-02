0beccc537051876f3906e0785e135b04c13099b7
Danilo Oliveira <danilo.av@gmail.com>
2019-01-02 01:12:30 -0200

    configurações ldap spring

--- src/main/java/br/gov/caixa/jsigml/config/WebSecurityConfig.java
+++ src/main/java/br/gov/caixa/jsigml/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().fullyAuthenticated().and().formLogin();
@@ -16,9 +15,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
 		auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource()
 				.url("ldap://localhost:8389/dc=springframework,dc=org").and().passwordCompare()
-				.passwordEncoder(new LdapShaPasswordEncoder()).passwordAttribute("userPassword");
-	}
+				.passwordEncoder(new LdapShaPasswordEncoder()).passwordAttribute("adminpassword");
 
+	}
 }
